efb456910a029d9dad6160c126e842ad924c9ef6
Anastasiia Antonova <antonova64@gmail.com>
2019-04-13 15:48:04 +0200

    test loop

--- src/components/hello-world/HelloWorld.ts
+++ src/components/hello-world/HelloWorld.ts
@@ -3,4 +3,16 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
     @Prop() private msg!: string;
+
+    message: string = 'Hello my dear friend! :)';
+
+    groceryList = [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ];
+
+    onClick (): void {
+        window.alert(this.groceryList[0].text)
+    }
 }
