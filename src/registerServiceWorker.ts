d7124b657ff4fccf8ca89eda7ddedfc39450605e
Anastasiia Antonova <antonova64@gmail.com>
2019-04-13 14:49:58 +0200

    split component into separate files

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -5,9 +5,9 @@ import { register } from 'register-service-worker';
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
-      console.log(
+        console.log(
         'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB',
+            'For more details, visit https://goo.gl/AFskqB',
       );
     },
     registered() {
